db634a52f0d17508d43258d57009ffa2f7e1fc59
Mxgxxl11 <miguel11.macj@gmail.com>
2024-11-10 22:01:09 -0500

    Aregar Key

--- flutter_application_2/lib/modos_juegos.dart
+++ flutter_application_2/lib/modos_juegos.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ModosJuego extends StatelessWidget {
+  const ModosJuego({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
