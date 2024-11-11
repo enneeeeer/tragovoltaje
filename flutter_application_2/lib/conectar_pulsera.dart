685b555933391dd8c4a431cac68f3eceac0a8bc0
Mao V.C <145241215+lyduana@users.noreply.github.com>
2024-11-10 21:49:41 -0500

    Update conectar_pulsera.dart

--- flutter_application_2/lib/conectar_pulsera.dart
+++ flutter_application_2/lib/conectar_pulsera.dart
@@ -6,12 +6,56 @@ class ConectarPulsera extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Ventana para conectar pulsera',
-        style: TextStyle(
-          color: Colors.white,
-        )
-      )
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16.0),
+            color: Colors.amber[700], 
+            child: Text(
+              'CONECTA TU PULSERA',
+              style: TextStyle(
+                color: Colors.black, 
+                fontSize: 24, 
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 25), 
+          Image.asset(
+            'assets/blutu.png', // Ruta 
+            width: 150, //  ancho 
+            height: 150, // altura
+          ),
+          SizedBox(height: 25), 
+          Text(
+            'Activa tu Bluetooth',
+            style: TextStyle(
+              color: Colors.white, 
+              fontSize: 18, 
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+              backgroundColor: Colors.amber[700], 
+            ),
+            onPressed: () {
+              // Boton sin funcionamiento por ahora
+            },
+            child: Text(
+              'CONECTAR',
+              style: TextStyle(
+                color: Colors.black, 
+                fontSize: 24, 
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
