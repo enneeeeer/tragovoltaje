2574838890ef9aa040e5f4c1690927248f44fb88
Mxgxxl11 <miguel11.macj@gmail.com>
2024-11-21 00:53:23 -0500

    Activar bluetooth y diseño de la pagina de control

--- flutter_application_2/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_application_2/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_blue_plus
 import in_app_purchase_storekit
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
