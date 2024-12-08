e46ca68aea9b6483a3d05fd3ebf84563f43a7efb
Mxgxxl11 <miguel11.macj@gmail.com>
2024-12-08 01:15:42 -0500

    Ads reales e integracion con Firebase

--- flutter_application_2/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_application_2/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import flutter_blue_plus
 import geolocator_apple
 import in_app_purchase_storekit
@@ -13,6 +14,7 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
